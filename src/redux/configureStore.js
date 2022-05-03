305e3eea4a7b8582ae93217540028f69306fec90
IfzaRasool <ifzarasoolarain@gmail.com>
2022-05-04 00:09:14 +0300

    create actions and reducres

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,1 +1,13 @@
-import { combineReducers } from 'redux';
+import Redux from 'redux';
+import bookReducer from './books/books';
+import categoryReducer from './categories/categories';
+
+const reducer = Redux.combineReducers({
+  bookReducer,
+  categoryReducer,
+
+});
+
+const store = Redux.ConfigureStore(reducer);
+
+export default store;
