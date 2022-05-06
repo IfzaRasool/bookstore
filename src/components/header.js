d28ac8e61060b4dcf4914d11895195215a607d4a
IfzaRasool <ifzarasoolarain@gmail.com>
2022-05-06 21:14:26 +0300

    Add Styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { VscAccount } from 'react-icons/vsc';
 
 const Header = () => (
   <div>
@@ -9,6 +10,9 @@ const Header = () => (
         <Link to="/" className="header-primary-heading">BOOKS</Link>
         <Link to="/category" className="header-secondary-heading">CATEGORIES</Link>
       </nav>
+      <h2 className="login">
+        <VscAccount />
+      </h2>
     </header>
   </div>
 );
